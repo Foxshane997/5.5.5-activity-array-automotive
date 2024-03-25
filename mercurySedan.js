ebb00818c06a44ea99beba79b994d6d8cfe8e88f
“Foxshane997” <Foxshane997@gmail.com>
2024-03-25 15:05:57 -0700

    adding fixes from john

--- mercurySedan.js
+++ mercurySedan.js
@@ -1,79 +1,56 @@
-//this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
-
-//this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+const VehicleModule = require('./vehicle').Vehicle
 
 class Car extends VehicleModule {
-    constructor(make, model, year, color, mileage){
-        super(make, model, year, color, mileage)
-        this.maximumPassengers = 6; 
-        this.passengers = 0;
-        this.numberOfWheels = 4; 
-        this.maximumSpeed = 160;
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 160;
         this.fuel = 10;
         this.scheduleService = false;
     }
-}
 
-    loadPassengers(num)
-        if(this.passengers < this.maximumPassengers){
-            if ((num + this.passengers) <= this.maximumPassengers){  /*Need to reference the solution for this. */
-                this.passengers = num;                               /*It was tricky and a litle hard to wrap my head around tbh. */
-                return this.passengers;
-            } 
-            else{
-                console.log(this.model + " " + this.make + "is at capacity");
-            }
+    checkService() {
+        if (this.mileage > 30000) {            
+            this.scheduleService = true
+            return this.scheduleService;                       
         }
-    
+    }
 
-    start()
-        if(this.fuel > 0){
-            console.log("The engine has started!")
-            return this.started = true;     
-        }                                   /*I referenced veicleBaseClass.js here for the this.start I believe that was required  */
-        else{
-            console.log("Gas tank is empty!")
+    start() {
+        if (this.fuel > 0) {            
+            console.log("engine has started.");
+            return this.started = true
+        } else {
+            console.log("no fuel");
             return this.started = false;
         }
-    
+    }
 
-    checkService() 
-        if(this.mileage > 25000){
-            this.scheduleService = true
-            return this.scheduleService;
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;               
+            } else {
+                console.log(this.model + " " + this.make + " not have enough space to take all passengers.");
+
+            }
+        } else {
+            console.log(this.model + " " + this.make + " is full");
         }
-
-    
-
+    }
 
 
-//TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
+}
 
-let myCar = new Car('mercury', 'Suped-sedan', '2005', 'black', 50000)
+
+let myCar = new Car('mercury', 'rad_sedan', '2002', 'white', 50000)
 
 myCar.start()
-myCar.loadPassenger(6)
+myCar.loadPassenger(5)
 myCar.stop()
 myCar.checkService()
 
 console.log(myCar)
-
-
-
-
-
-
-
-
-//TO DO: Creating Instances and Testing Them
-
-//You can use the same instance "v" of the Vehicle class above for the base class.
-
-
-
-
-
-//Create at least two new instances of the Car class and test them here:
